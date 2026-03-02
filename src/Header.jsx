ba506ec3f8aa208ecfaa63af393df9de26c72d56
zamanbek <zhubanhs@gmail.com>
2026-03-02 07:44:25 +0500

    Changed name of the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
 
     return(
         <header>
-            <h1 className="header-title">История Казахского Края</h1>
+            <h1 className="header-title">"История Казахстана XVIII - начала XX вв. в документах и материалах Российской империи" (практикум)</h1>
             <nav className="header-nav">
                 <a href="#">Главная</a>
                 <a href="#">Учебный комплекс</a>
